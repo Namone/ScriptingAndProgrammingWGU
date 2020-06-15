770442af8391c77fe8b2627b18957cd304059d76
Namone <al2xnord@yahoo.com>
2020-06-15 17:15:00 -0600

    Progress

--- WGU C++ Project/Roster.cpp
+++ WGU C++ Project/Roster.cpp
@@ -7,3 +7,21 @@ Roster::Roster() {
 Roster::~Roster() {
 
 }
+
+void Roster::add(
+    std::string studentID, 
+    std::string firstName, 
+    std::string lastName, 
+    std::string emailAddress, 
+    int age, 
+    int daysInCourse1, 
+    int daysInCourse2, 
+    int daysInCourse3, 
+    DegreeProgram degreeProgram
+) {
+
+}
+
+void Roster::remove(std::string studentId) {
+
+}
--- WGU C++ Project/Roster.h
+++ WGU C++ Project/Roster.h
@@ -1,8 +1,33 @@
 #pragma once
+#include "Student.h"
+
 class Roster
 {
 public:
 	Roster();
 	~Roster();
+
+	void add(
+		std::string studentID, 
+		std::string firstName, 
+		std::string lastName, 
+		std::string emailAddress, 
+		int age, 
+		int daysInCourse1, 
+		int daysInCourse2, 
+		int daysInCourse3, 
+		DegreeProgram degreeProgram
+	);
+	void remove(std::string studentId);
+	void printByDegreeProgram(DegreeProgram degreeProgram);
+	void printAverageDaysInCourse(std::string studentId);
+
+	void printAll();
+	void printInvalidEmails();
+protected:
+	Student** getStudents() { return students; };
+	void setStudents(Student** newStudents) { students = newStudents; };
+private:
+	Student** students = new Student*[5];
 };
 
